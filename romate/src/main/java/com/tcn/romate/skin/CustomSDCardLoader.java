c724d64f5443d5c1015c86935ae0ab2b0bfebb6d
wangfeng <745319870@qq.com>
2021-04-26 13:51:04 +0800

    '11'

--- romate/src/main/java/com/tcn/romate/skin/CustomSDCardLoader.java
+++ romate/src/main/java/com/tcn/romate/skin/CustomSDCardLoader.java
@@ -2,6 +2,8 @@ package com.tcn.romate.skin;
 
 import android.content.Context;
 
+import com.blankj.utilcode.util.Utils;
+
 import java.io.File;
 
 import skin.support.load.SkinSDCardLoader;
@@ -10,9 +12,16 @@ import skin.support.utils.SkinFileUtils;
 public class CustomSDCardLoader extends SkinSDCardLoader {
     public static final int SKIN_LOADER_STRATEGY_SDCARD = Integer.MAX_VALUE;
 
+    public static String m_strAdvertPath = "/TcnFolder/skins";
+
     @Override
     protected String getSkinPath(Context context, String skinName) {
-        return new File(SkinFileUtils.getSkinDir(context), skinName).getAbsolutePath();
+        String mExternalPath = Util.getExternalStorageDirectory();
+        if (null == mExternalPath) {
+            return "";
+        }
+        String folderPath = mExternalPath + m_strAdvertPath;
+        return new File(folderPath, skinName).getAbsolutePath();
     }
 
     @Override
